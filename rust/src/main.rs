8be795c32ca512e571a45238fcab0c03820ccb2b
ajassi9751 <ajassi9751@gmail.com>
2026-04-20 12:55:24 +0000

    Use bufwriter to write to stdout

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,7 @@
 mod pidcontroller;
+
+use std::io::{self, BufWriter, Write};
+
 use pidcontroller::PIDController;
 
 const TARGETPOS: f64 = 10.0;
@@ -7,8 +10,11 @@ const I: f64 = 0.1;
 const D: f64 = 0.005;
 
 fn main() {
+    let stdout = io::stdout().lock();
+    let mut writer = BufWriter::new(stdout);
     let mut pidc = PIDController::new(P, I, D, None);
     for i in 0..20 {
-        println!("{}", pidc.calculate(i as f64,TARGETPOS));
+        let _ = writeln!(writer, "{}", pidc.calculate(i as f64, TARGETPOS));
     }
+    let _ = writer.flush();
 }
